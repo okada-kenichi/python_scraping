7de19fdc6f99d273a03c704765bcabf26bdce332
okada-kenichi <71626308+okada-kenichi@users.noreply.github.com>
2021-10-17 12:59:30 +0900

    コメントアウトの追記のみ

--- bs4_test01.py
+++ bs4_test01.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = "https://ja.wikipedia.org"
 response = requests.get(url)
-
+# httpヘッダ編集していない
 soup = BeautifulSoup(response.content, "html.parser")
 today = soup.find("div", attrs={"id":"on_this_day"}).text
 print(today)
